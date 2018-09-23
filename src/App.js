c4883084c1ad9b836328ef7b87800086eb775146
Bethany Amphlett <bethany.amphlett@gmail.com>
2018-09-23 19:14:52 +0200

    checkpoint-2-list-chat-rooms: completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA_hLcJL-kc0j2p6UPXA-AA0VmFau5KV-4",
+    authDomain: "bloc-chat-react-387bc.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-387bc.firebaseio.com",
+    projectId: "bloc-chat-react-387bc",
+    storageBucket: "bloc-chat-react-387bc.appspot.com",
+    messagingSenderId: "44400581391"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <h1>Bloc Chat</h1>
+        <h2>See our rooms below</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import RoomList from './components/RoomList';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
